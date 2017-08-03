c2e43058880c516ce1a915063eb5901e97f4f0f4
Taylor Salo <tsalo006@fiu.edu>
2017-08-03 16:22:47 -0400

    Add tests and files for tests.

--- gclda/tests/test_decode.py
+++ gclda/tests/test_decode.py
@@ -3,3 +3,50 @@
 """
 Tests for GC-LDA decode module.
 """
+from os.path import join
+
+from gclda.model import Model
+from gclda.decode import Decoder
+from gclda.tests.utils import get_test_data_path
+
+
+def test_init():
+    """Smoke test for Decoder class.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    model = Model.load(model_file)
+    decoder = Decoder(model)
+    assert isinstance(decoder, Decoder)
+
+
+def test_decode_roi():
+    """Acceptance test of ROI-based decoding.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    roi_file = join(get_test_data_path(), 'roi.nii.gz')
+    model = Model.load(model_file)
+    decoder = Decoder(model)
+    decoded_df = decoder.decode_roi(roi_file)
+    assert decoded_df.shape[0] == model.n_word_labels
+
+
+def test_decode_continuous():
+    """Acceptance test of continuous image-based decoding.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    continuous_file = join(get_test_data_path(), 'continuous.nii.gz')
+    model = Model.load(model_file)
+    decoder = Decoder(model)
+    decoded_df = decoder.decode_continuous(continuous_file)
+    assert decoded_df.shape[0] == model.n_word_labels
+
+
+def test_encode():
+    """Acceptance test of test-to-image encoding.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    text = 'painful stimulation during a language task'
+    model = Model.load(model_file)
+    decoder = Decoder(model)
+    encoded_matrix = decoder.encode(text)
+    assert encoded_matrix.shape == model.dataset.masker.volume.shape
--- gclda/tests/test_model.py
+++ gclda/tests/test_model.py
@@ -3,3 +3,41 @@
 """
 Tests for GC-LDA model module.
 """
+from os import remove
+from os.path import join, isfile
+
+from gclda.model import Model
+from gclda.dataset import Dataset
+from gclda.tests.utils import get_test_data_path
+
+
+def test_init():
+    """Smoke test for Model class."""
+    dataset_file = join(get_test_data_path(), 'gclda_dataset.pkl')
+    dset = Dataset.load(dataset_file)
+    model = Model(dset, n_topics=50, n_regions=1, symmetric=False,
+                  alpha=.1, beta=.01, gamma=.01, delta=1.,
+                  dobs=25, roi_size=10., seed_init=1)
+    assert isinstance(model, Model)
+
+
+def test_load_model():
+    """Ensure model can be loaded from pickled file.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    model = Model.load(model_file)
+    assert isinstance(model, Model)
+
+
+def test_save_model():
+    """Ensure dataset can be saved to pickled file.
+    """
+    model_file = join(get_test_data_path(), 'gclda_model.pkl')
+    temp_file = join(get_test_data_path(), 'temp.pkl')
+    model = Model.load(model_file)
+    model.save(temp_file)
+    file_found = isfile(temp_file)
+    assert file_found
+
+    # Perform cleanup
+    remove(temp_file)
