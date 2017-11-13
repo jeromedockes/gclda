b71ecfbdd6ef59169edc0b524057b4ee04634513
Taylor Salo <tsalo006@fiu.edu>
2017-11-13 12:49:00 -0500

    Reorganize decode documentation.

--- gclda/decode.py
+++ gclda/decode.py
@@ -23,16 +23,10 @@ def decode_roi(model, roi, topic_priors=None, prior_weight=1.):
     Perform image-to-text decoding for discrete image inputs (e.g., regions
     of interest, significant clusters).
 
-    1.  Compute p_topic_g_voxel.
-            - From Model
-    2.  Compute topic weight vector (tau_t).
-            - topic_weights = np.sum(p_topic_g_voxel, axis=1) (across voxels)
-    3.  Multiply tau_t by topic-by-word matrix (p_word_g_topic).
-    4.  The resulting vector (tau_t*p_word_g_topic) should be word weights
-        for your selected studies.
-
     Parameters
     ----------
+    model : :obj:`gclda.model.Model`
+        Model object needed for decoding.
     roi : :obj:`nibabel.Nifti1Image` or str
         Binary image to decode into text. If string, path to a file with
         the binary image.
@@ -49,6 +43,16 @@ def decode_roi(model, roi, topic_priors=None, prior_weight=1.):
         A DataFrame with the word-tokens and their associated weights.
     topic_weights : :obj:`numpy.ndarray` of :obj:`float`
         The weights of the topics used in decoding.
+
+    Notes
+    -----
+    1.  Compute p_topic_g_voxel.
+            - From Model
+    2.  Compute topic weight vector (tau_t).
+            - topic_weights = np.sum(p_topic_g_voxel, axis=1) (across voxels)
+    3.  Multiply tau_t by topic-by-word matrix (p_word_g_topic).
+    4.  The resulting vector (tau_t*p_word_g_topic) should be word weights
+        for your selected studies.
     """
     if isinstance(roi, str):
         roi = nib.load(roi)
@@ -94,16 +98,10 @@ def decode_continuous(model, image, topic_priors=None, prior_weight=1.):
     Perform image-to-text decoding for continuous inputs (e.g.,
     unthresholded statistical maps).
 
-    1.  Compute p_topic_g_voxel.
-    2.  Compute topic weight vector (tau_t) by multiplying p_topic_g_voxel
-        by input image.
-    3.  Multiply tau_t by topic-by-word matrix (p_word_g_topic).
-    4.  The resulting vector (tau_t*p_word_g_topic) should be word weights
-        for your map, but the values are scaled based on the input image, so
-        they won't necessarily mean much.
-
     Parameters
     ----------
+    model : :obj:`gclda.model.Model`
+        Model object needed for decoding.
     image : :obj:`nibabel.Nifti1Image` or str
         Whole-brain image to decode into text. Must be in same space as
         model and dataset. Model's template available in
@@ -121,6 +119,17 @@ def decode_continuous(model, image, topic_priors=None, prior_weight=1.):
         A DataFrame with the word-tokens and their associated weights.
     topic_weights : :obj:`numpy.ndarray` of :obj:`float`
         The weights of the topics used in decoding.
+
+    Notes
+    -----
+    1.  Compute p_topic_g_voxel.
+    2.  Compute topic weight vector (tau_t) by multiplying p_topic_g_voxel
+        by input image.
+    3.  Multiply tau_t by topic-by-word matrix (p_word_g_topic).
+    4.  The resulting vector (tau_t*p_word_g_topic) should be word weights
+        for your map, but the values are scaled based on the input image, so
+        they won't necessarily mean much.
+
     """
     if isinstance(image, str):
         image = nib.load(image)
@@ -155,40 +164,10 @@ def encode(model, text, out_file=None, topic_priors=None, prior_weight=1.):
     r"""
     Perform text-to-image encoding.
 
-    Notes
-    -----
-
-    The matrix p_topic_g_word is computed from the input text
-
-    Terminology:
-
-    .. math ::
-        P_{tw}: Matrix of
-
-    .. math ::
-        P_{tw} = p(t = i|w = j)
-
-        P_{tw} = \frac{p(w = j|t = i) * p(t = i)}{p(w)}
-
-        P_{tw} = \frac{\Phi_{ji}}{\sum_{i=1}^{T} \Phi_{ji}}
-
-        \tau_{t} = \sum_{w} P_{tw}
-
-        values = \tau_{t} \cdot A
-
-    1.  Compute p_topic_g_word.
-            - p_topic_g_word = p_word_g_topic * p_topic / p_word
-            - p_topic is uniform (1/n topics)
-    2.  Compute topic weight vector (tau_t).
-            - tau_t = np.sum(p_topic_g_word, axis=1) (across words)
-    3.  Multiply tau_t by topic-by-voxel matrix of smoothed p_voxel_g_topic
-        (A; not sure where it is, but I don't think it's the same as A in
-        model.py).
-    4.  The resulting map (tau_t*A) is the encoded image. Values are *not*
-        probabilities.
-
     Parameters
     ----------
+    model : :obj:`gclda.model.Model`
+        Model object needed for decoding.
     text : str or list
         Text to encode into an image.
     out_file : str, optional
@@ -206,6 +185,43 @@ def encode(model, text, out_file=None, topic_priors=None, prior_weight=1.):
         The encoded image.
     topic_weights : :obj:`numpy.ndarray` of :obj:`float`
         The weights of the topics used in encoding.
+
+    Notes
+    -----
+    The matrix p_topic_g_word is computed from the input text
+
+    Terminology:
+
+    .. math ::
+        P_{tw}: Matrix of stuff
+
+    Derive topic weights:
+
+    .. math ::
+        P_{tw} = p(t = i|w = j)
+
+        P_{tw} = \frac{p(w = j|t = i) * p(t = i)}{p(w)}
+
+        P_{tw} = \frac{\Phi_{ji}}{\sum_{i=1}^{T} \Phi_{ji}}
+
+        \tau_{t} = \sum_{w} P_{tw}
+
+    Derive voxel weights:
+
+    .. math ::
+        values = \tau_{t} \cdot A
+
+    Summary:
+    1.  Compute p_topic_g_word.
+            - p_topic_g_word = p_word_g_topic * p_topic / p_word
+            - p_topic is uniform (1/n topics)
+    2.  Compute topic weight vector (tau_t).
+            - tau_t = np.sum(p_topic_g_word, axis=1) (across words)
+    3.  Multiply tau_t by topic-by-voxel matrix of smoothed p_voxel_g_topic
+        (A; not sure where it is, but I don't think it's the same as A in
+        model.py).
+    4.  The resulting map (tau_t*A) is the encoded image. Values are *not*
+        probabilities.
     """
     if isinstance(text, list):
         text = ' '.join(text)
