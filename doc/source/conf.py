12a71b9273be5edc2528460f7b0aa6fc4c033ab8
Taylor Salo <tsalo006@fiu.edu>
2018-05-13 21:07:29 -0400

    Undo tests.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,13 +15,6 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath('./sphinxext'))
-
-try:
-    import sphinxext
-except:
-    raise Exception('{0}\n\n{1}'.format(sys.path, os.path.abspath('./sphinxext')))
-
 import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
